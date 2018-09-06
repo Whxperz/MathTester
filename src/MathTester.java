d26a015e0b46043f655c1b43a229f590b1388b8e
Jayasri <Jayasri.Sundararajan.2022@student.lhschools.org>
2018-09-06 14:18:39 -0400

    made changes to class

--- src/MathTester.java
+++ src/MathTester.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
-
 public class MathTester {
+
+    public static void main (String[] args){
+        String firstWord;
+        String secondWord;
+        int firstNumber,secondNumber,total;
+
+
+        firstWord = "dog";
+        secondWord = "house";
+
+        firstNumber = 5;
+        secondNumber = 10;
+        total = firstNumber+secondNumber;
+        System.out.println(total);
+        total = total + 10;
+        System.out.println(total);
+       /* System.out.println(firstWord+secondWord);
+        System.out.println(firstNumber+secondNumber);
+        System.out.println("firstNumber + secondNumber is "+ (firstNumber+secondNumber));
+        System.out.println("firstNumber - secondNumber is "+ (firstNumber-secondNumber));
+        System.out.println("secondNumber /  firstNumber is "+ (secondNumber/firstNumber));
+        System.out.println("firstNumber * secondNumber is "+ (firstNumber*secondNumber));
+        System.out.println("The modulus of the secondNumber by the firstNumber" +
+               " is "+ (secondNumber%firstNumber));*/
+    }
 }
