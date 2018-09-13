a12fd0d3c73dab2fd89c0b48283d7d4826182e40
Jayasri <Jayasri.Sundararajan.2022@student.lhschools.org>
2018-09-13 13:43:05 -0400

    Added statements for conditional operations

--- src/MathTester.java
+++ src/MathTester.java
@@ -1,7 +1,18 @@
 public class MathTester {
 
     public static void main (String[] args){
-        String firstWord;
+        int firstNumber = 10;
+        int secondNumber = 20;
+        if(firstNumber>secondNumber){
+            System.out.println(firstNumber +" is less");
+        }
+        else if(firstNumber<0){
+            System.out.println(firstNumber + " is greater than 0");
+        }
+        else{
+            System.out.println("I don't know what you want");
+        }
+        /*String firstWord;
         String secondWord;
         int firstNumber,secondNumber,total;
 
@@ -14,7 +25,7 @@ public class MathTester {
         total = firstNumber+secondNumber;
         System.out.println(total);
         total = total + 10;
-        System.out.println(total);
+        System.out.println(total);*/
        /* System.out.println(firstWord+secondWord);
         System.out.println(firstNumber+secondNumber);
         System.out.println("firstNumber + secondNumber is "+ (firstNumber+secondNumber));
